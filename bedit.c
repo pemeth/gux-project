3e2bedda3137864835419ddc0b3b2c17852b71a2
Patrik Nemeth <patriknemethpn@gmail.com>
2022-01-16 00:52:12 +0100

    Add call to delete list of Bezier curves before program exit

--- bedit.c
+++ bedit.c
@@ -312,5 +312,7 @@ int main(int argc, char *argv[])
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
 
+    delete_list(&(info.list));
+
     return status;
 }
