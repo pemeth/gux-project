d3584361d2c2a8a2297b7951923d60fd3a5281a1
Patrik Nemeth <patriknemethpn@gmail.com>
2022-01-18 14:36:47 +0100

    Add help mneu with usage information

--- bedit.c
+++ bedit.c
@@ -11,6 +11,28 @@
     #define M_PI 3.14159265358979323846
 #endif
 
+#define USAGE_HELP_TEXT "\
+\nSHORTCUTS\
+\n\t\"A\"\t\tAdd a new curve by defining its four control points.\
+\n\t\"D\"\t\tDelete a curve.\
+\n\t\"S\"\t\tToggle showing/hiding of control points.\
+\n\t\"Ctrl+N\"\tCreate a new blank canvas.\
+\n\t\"Ctrl+Q\"\tQuit the application.\
+\n\
+\nCREATING NEW CURVES\
+\n\tTo create a new Bezier curve use shortcut \"A\" and define four\
+\n\tBezier control points by clicking on the canvas.\
+\n\
+\nEDITING A CURVE\
+\n\tClick and drag any control point. The control points must be\
+\n\tvisible (toggle with \"S\" if they are not).\
+\n\
+\nSELECTING A CURVE\
+\n\tTo select a curve, click on any of its control points. The selected \
+\n\tcurve's control points are tinted red. To deselect a curve,\
+\n\tclick anywhere on the canvas other than a control point.\
+\n"
+
 // TODO probably move these to a header
 
 typedef struct RuntimeInfo
@@ -211,6 +233,37 @@ static gboolean canvas_button_move(GtkWidget* self, GdkEventMotion* event, Runti
     return TRUE;
 }
 
+static void show_help_widnow(GtkWidget *self, gpointer *data)
+{
+    GtkWidget *helpWindow, *helpTextView, *box;
+    GtkTextBuffer *helpTextBuffer;
+
+    helpTextView = gtk_text_view_new();
+    helpTextBuffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(helpTextView));
+
+    gtk_text_buffer_set_text(helpTextBuffer, USAGE_HELP_TEXT, -1);
+
+    helpWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+    gtk_window_set_title(GTK_WINDOW(helpWindow), "Usage Help");
+    gtk_window_set_default_size(GTK_WINDOW(helpWindow), 320, 200);
+
+    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_container_add(GTK_CONTAINER(helpWindow), box);
+
+    // Make the helptext read only
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(helpTextView), FALSE);
+    gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(helpTextView), FALSE);
+
+    gtk_widget_set_margin_start(helpTextView, 5);
+    gtk_widget_set_margin_end(helpTextView, 5);
+    gtk_widget_set_margin_top(helpTextView, 5);
+    gtk_widget_set_margin_bottom(helpTextView, 5);
+
+    gtk_box_pack_start(GTK_BOX(box), helpTextView, FALSE, FALSE, 0);
+    gtk_widget_show_all(helpWindow);
+}
+
 static void toggle_show_control_points(GtkWidget* self, RuntimeInfo *data)
 {
     data->flagShowControlPoints = data->flagShowControlPoints ? FALSE : TRUE;
@@ -228,11 +281,11 @@ static void quit_app(GtkWidget* self, RuntimeInfo *data)
 static void activate(GtkApplication *app, RuntimeInfo *data)
 {
     GtkWidget *window, *box, *canvas;
-    GtkWidget *menubar, *fileMenu, *editMenu;
+    GtkWidget *menubar, *fileMenu, *editMenu, *helpMenu;
     /* Menu Items */
-    GtkWidget *quitMI, *addCurveMI, *showControlPointsMI, *newCanvasMI;
+    GtkWidget *quitMI, *addCurveMI, *showControlPointsMI, *newCanvasMI, *usageMI;
     /* Top Level Menu Items */
-    GtkWidget *fileTLMI, *editTLMI;
+    GtkWidget *fileTLMI, *editTLMI, *helpTLMI;
 
     GtkAccelGroup *accel_group;
 
@@ -255,15 +308,17 @@ static void activate(GtkApplication *app, RuntimeInfo *data)
     /* Menus */
     fileMenu = gtk_menu_new();
     editMenu = gtk_menu_new();
+    helpMenu = gtk_menu_new();
 
     /* Menu Items */
     fileTLMI = gtk_menu_item_new_with_mnemonic("_File");
     editTLMI = gtk_menu_item_new_with_mnemonic("_Edit");
+    helpTLMI = gtk_menu_item_new_with_mnemonic("_Help");
     quitMI = gtk_menu_item_new_with_mnemonic("_Quit");
     addCurveMI = gtk_menu_item_new_with_mnemonic("_Add Curve");
     showControlPointsMI = gtk_menu_item_new_with_mnemonic("_Show Control Points");
     newCanvasMI = gtk_menu_item_new_with_mnemonic("_New Canvas");
-    // TODO add new menu with help
+    usageMI = gtk_menu_item_new_with_mnemonic("_Usage");
 
     /* Menu encapsulation */
     gtk_menu_item_set_submenu(GTK_MENU_ITEM(fileTLMI), fileMenu);
@@ -276,6 +331,10 @@ static void activate(GtkApplication *app, RuntimeInfo *data)
     gtk_menu_shell_append(GTK_MENU_SHELL(editMenu), addCurveMI);
     gtk_menu_shell_append(GTK_MENU_SHELL(editMenu), showControlPointsMI);
     gtk_menu_shell_append(GTK_MENU_SHELL(menubar), editTLMI);
+
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(helpTLMI), helpMenu);
+    gtk_menu_shell_append(GTK_MENU_SHELL(helpMenu), usageMI);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menubar), helpTLMI);
 
     gtk_box_pack_start(GTK_BOX(box), menubar, FALSE, FALSE, 0);
 
@@ -298,6 +357,8 @@ static void activate(GtkApplication *app, RuntimeInfo *data)
         G_CALLBACK(adding_curve), data);
     g_signal_connect(G_OBJECT(showControlPointsMI), "activate",
         G_CALLBACK(toggle_show_control_points), data);
+    g_signal_connect(G_OBJECT(usageMI), "activate",
+        G_CALLBACK(show_help_widnow), NULL);
 
     /********* CANVAS ********/
     /*************************/
